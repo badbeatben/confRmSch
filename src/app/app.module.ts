a8fc43ad6b83a93c8b0576bbba86ceed63de1a04
Ben <ben@kratesoft.com>
2021-10-28 12:07:20 -0400

    changing event modal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,11 @@
 import 'flatpickr/dist/flatpickr.css';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { CalendarModule, DateAdapter } from 'angular-calendar';
-import { FlatpickrModule } from 'angularx-flatpickr';
 import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
 import { FormsModule } from '@angular/forms';
 import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
@@ -19,11 +18,10 @@ import { CalendarComponent } from './pages/calendar/calendar.component';
   ],
   imports: [
     BrowserModule,
-    CommonModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     FormsModule,
     NgbModalModule,
-    FlatpickrModule.forRoot(),
     CalendarModule.forRoot({ provide: DateAdapter, useFactory: adapterFactory })
   ],
   providers: [],
