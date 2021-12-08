16a7f07e1476a3117cdda465237fb57092cbd9e9
Ben <ben@kratesoft.com>
2021-12-08 15:54:13 -0500

    Final touches for forgot-password page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CalendarComponent } from './pages/calendar/calendar.component';
+import { ForgotPasswordComponent } from './pages/forgot-password/forgot-password.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'forgot-password', component: ForgotPasswordComponent },
   { path: 'calendar', component: CalendarComponent },
   { path: '', redirectTo: '/calendar', pathMatch: 'full' },
   { path: '**', redirectTo: '/calendar' }
